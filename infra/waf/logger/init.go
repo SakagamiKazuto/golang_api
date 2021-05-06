3ed5e9a7dd70f11363ba10aa293adf13b1a40d13
SakagamiKazuto <carpwin115@icloud.com>
2021-05-06 15:36:15 +0900

    loglevel分岐するように

--- infra/waf/logger/init.go
+++ infra/waf/logger/init.go
@@ -2,12 +2,20 @@ package logger
 
 import (
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 var Log = NewLogger()
 
 func init() {
 	defer Log.Info("Logger Settints is set")
-	// JSONフォーマット
+
 	Log.SetFormat(&logrus.JSONFormatter{})
+
+	switch os.Getenv("APP_MODE") {
+	case "production", "staging":
+		Log.SetLogLevel(logrus.InfoLevel)
+	default:
+		Log.SetLogLevel(logrus.DebugLevel)
+	}
 }
